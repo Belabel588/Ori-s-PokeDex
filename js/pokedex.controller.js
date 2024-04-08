edc69cca9b300cf7ed8f9f440dd45fbe052ede2b
Belabel588 <orih1402@gmail.com>
2024-04-08 22:06:34 +0300

    fix  render function is in the service inside the axios function - seperate and move to controller

--- js/pokedex.controller.js
+++ js/pokedex.controller.js
@@ -18,3 +18,25 @@ function onPokemonLoad() {
   elLoader.classList.add('hidden')
   elPokedex.style.display = 'block'
 }
+
+function onRenderPokedex(response) {
+
+  const elPokemonSprite = response.data.sprites.front_default
+  const elImg = document.getElementById('pokemonSprite')
+  elImg.src = elPokemonSprite
+  elImg.style.display = 'block'
+
+  const elPokemonName = document.querySelector('.pokemon-name')
+  const elPokemonId = document.querySelector('.pokemon-id')
+  const elPokemonType = document.querySelector('.pokemon-type')
+  const elPokemonAbilities = document.querySelector('.pokemon-abilities')
+  const elPokemonHeight = document.querySelector('.pokemon-height')
+  const elPokemonWeight = document.querySelector('.pokemon-weight')
+
+  elPokemonName.innerText = response.data.name
+  elPokemonId.innerText = '#' + response.data.id
+  elPokemonType.innerText = response.data.types.map(type => type.type.name).join(', ')
+  elPokemonAbilities.innerText = response.data.abilities.map(ability => ability.ability.name).join(', ')
+  elPokemonHeight.innerText = response.data.height
+  elPokemonWeight.innerText = response.data.weight
+}
--- js/services/pokedex.service.js
+++ js/services/pokedex.service.js
@@ -1,30 +1,9 @@
 'use strict'
 
 function getPokemon(pokemon) {
-  axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemon}`)
+  const pokedex = axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemon}`)
 
-    .then(response => {
-      const elPokemonSprite = response.data.sprites.front_default
-      const elImg = document.getElementById('pokemonSprite')
-      elImg.src = elPokemonSprite
-      elImg.style.display = 'block'
-
-      const elPokemonName = document.querySelector('.pokemon-name')
-      const elPokemonId = document.querySelector('.pokemon-id')
-      const elPokemonType = document.querySelector('.pokemon-type')
-      const elPokemonAbilities = document.querySelector('.pokemon-abilities')
-      const elPokemonHeight = document.querySelector('.pokemon-height')
-      const elPokemonWeight = document.querySelector('.pokemon-weight')
-
-      elPokemonName.innerText = response.data.name
-      elPokemonId.innerText = '#' + response.data.id
-      elPokemonType.innerText = response.data.types.map(type => type.type.name).join(', ')
-      elPokemonAbilities.innerText = response.data.abilities.map(ability => ability.ability.name).join(', ')
-      elPokemonHeight.innerText = response.data.height
-      elPokemonWeight.innerText = response.data.weight
-
-
-    })
+  pokedex.then(response => onRenderPokedex(response))
     .catch(error => {
 
       alert('There is no such Pokemon!')
@@ -34,4 +13,5 @@ function getPokemon(pokemon) {
 
     })
 }
+
 
